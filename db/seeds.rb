53aa0bdace488fb2f84c82cf7bbda68a4080dbfd
o281700 <siba0311@gmail.com>
2020-12-03 22:00:05 +0900

    Bookモデル/テーブル作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Book.create(title: 'BookTitle1', author: 'author1', impression: 'Please write your first impression')
+Book.create(title: 'BookTitle2', author: 'author2', impression: 'Please write your second impression')
+Book.create(title: 'BookTitle3', author: 'author3', impression: 'Please write your third impression')
+Book.create(title: 'BookTitle4', author: 'author4', impression: 'Please write your fourth impression')
+Book.create(title: 'BookTitle5', author: 'author5', impression: 'Please write your fifth impression')
+Book.create(title: 'BookTitle6', author: 'author6', impression: 'Please write your sixth impression')
+Book.create(title: 'BookTitle7', author: 'author7', impression: 'Please write your 7th impression')
+Book.create(title: 'BookTitle8', author: 'author8', impression: 'Please write your 8th impression')
+Book.create(title: 'BookTitle9', author: 'author9', impression: 'Please write your 9th impression')
+Book.create(title: 'BookTitle10', author: 'author10', impression: 'Please write your 10th impression')
