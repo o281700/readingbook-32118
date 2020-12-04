ca52b43e6a9bf61c11463cca73243e71a209344c
o281700 <siba0311@gmail.com>
2020-12-04 22:36:19 +0900

    投稿機能実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,8 +4,24 @@ class BooksController < ApplicationController
   end
 
   def new
+    @book = Book.new
   end
 
   def create
+    @book = Book.new(book_params)
+    if @book.valid?
+      @book.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(
+      :title, :author, :impression
+    ).merge(user_id: current_user.id)
   end
 end
