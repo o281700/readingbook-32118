b083cc7f270de240c6eb1f4260f917476655ca83
o281700 <siba0311@gmail.com>
2020-12-06 03:46:47 +0900

    user詳細画面実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
   def show
+    @nickname = current_user.nickname
+    @books = current_user.books.order("created_at DESC")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'books#index'
   resources :books, only: [:index, :new, :create, :show]
-  resources :user, only: [:show]
+  resources :users, only: [:show]
 end
