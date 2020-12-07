4b858e5f2305dce46fa5dc7878a309f6784b4a6b
o281700 <siba0311@gmail.com>
2020-12-07 23:11:12 +0900

    ヘッター/フッターテンプレート化

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def show
-    @nickname = current_user.nickname
-    @books = current_user.books.order("created_at DESC")
+    user = User.find(params[:id])
+    @nickname = user.nickname
+    @books = user.books.order("created_at DESC")
   end
 end
